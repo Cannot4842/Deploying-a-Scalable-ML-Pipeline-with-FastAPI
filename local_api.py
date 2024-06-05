011ad722eb4f371f0a7e918661f05348ab5a0bd4
Taimoor k <Taimoork14@outlook.com>
2024-06-04 23:55:37 -0400

    Add GET and POST requests in local_api.py for interacting with FastAPI server
    - Implement GET request to fetch welcome message from FastAPI server
    - Implement POST request to send sample data for inference to FastAPI server
    - Add error handling for POST request to print response text if JSON decoding fails
    - Print status code and response for both GET and POST requests

--- local_api.py
+++ local_api.py
@@ -1,16 +1,57 @@
-import json
+# import json
 
+# import requests
+
+# # TODO: send a GET using the URL http://127.0.0.1:8000
+# r = # Your code here
+
+# # TODO: print the status code
+# # print()
+# # TODO: print the welcome message
+# # print()
+
+
+
+# data = {
+#     "age": 37,
+#     "workclass": "Private",
+#     "fnlgt": 178356,
+#     "education": "HS-grad",
+#     "education-num": 10,
+#     "marital-status": "Married-civ-spouse",
+#     "occupation": "Prof-specialty",
+#     "relationship": "Husband",
+#     "race": "White",
+#     "sex": "Male",
+#     "capital-gain": 0,
+#     "capital-loss": 0,
+#     "hours-per-week": 40,
+#     "native-country": "United-States",
+# }
+
+# # TODO: send a POST using the data above
+# r = # Your code here
+
+# # TODO: print the status code
+# # print()
+# # TODO: print the result
+# # print()
+
+
+# adding new code
+import json
 import requests
 
+# URL of the FastAPI server
+url = "http://127.0.0.1:8000"
+
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = # Your code here
+r_get = requests.get(url)
 
 # TODO: print the status code
-# print()
+print(f"GET request status code: {r_get.status_code}")
 # TODO: print the welcome message
-# print()
-
-
+print(f"GET request response: {r_get.json()}")
 
 data = {
     "age": 37,
@@ -30,9 +71,12 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = # Your code here
+r_post = requests.post(f"{url}/inference/", json=data)
 
 # TODO: print the status code
-# print()
+print(f"POST request status code: {r_post.status_code}")
 # TODO: print the result
-# print()
+try:
+    print(f"POST request response: {r_post.json()}")
+except requests.exceptions.JSONDecodeError:
+    print(f"POST request response (text): {r_post.text}")
