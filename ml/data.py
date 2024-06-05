b6fd57bfdca69fe6de06271121c85d09e4b4ef7d
Taimoor k <Taimoork14@outlook.com>
2024-06-05 13:58:30 -0400

    Handle hyphenated column names in process_data function by replacing hyphens with underscores

--- ml/data.py
+++ ml/data.py
@@ -49,7 +49,7 @@ def process_data(
         X = X.drop([label], axis=1)
     else:
         y = np.array([])
-
+    categorical_features = [cf.replace("-", "_") for cf in categorical_features]
     X_categorical = X[categorical_features].values
     X_continuous = X.drop(*[categorical_features], axis=1)
 
